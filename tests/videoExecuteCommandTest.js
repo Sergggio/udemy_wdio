48c59e2ab0db5d3b2dfb765a0771d8d87946689a
Serggio <oceanite@yandex.ru>
2019-12-19 14:00:11 +0300

    Execute command - added new test videoExecuteCommandTest.js

---
+++ tests/videoExecuteCommandTest.js
@@ -0,0 +1,35 @@
+beforeEach(function() {
+	browser.url("https://www.w3schools.com/html/html5_video.asp");
+})
+
+//Injects a snippet of JavaScript into the page for execution in the context of the currently selected frame.
+browser.addCommand('isVideoPaused', function(){
+	
+	//anything outside the script cant access inside vise versa
+	var isPaused = browser.execute(function(){
+		console.log('Outputted inside the console window');
+		
+		//cant use browser command inside the script
+		var video = document.querySelector('#video1');
+		return video.paused;
+	})
+	return isPaused.value; //verify that the video is indeed paused
+});
+
+describe('Video test', function() {
+	it('Validate that the video is paused upon accessing the page', function(done) {
+		this.timeout(20000);
+		browser.pause(6000);
+		var isPaused = browser.isVideoPaused();
+		expect(isPaused).to.be.true;
+		browser.pause(6000);
+	});
+
+	it('Alter the width of the video', function(done) {
+		var videoWidth = browser.execute(function() {
+			var video = document.querySelector('#video1');
+			return video.style.width = "300px";
+		});
+		browser.pause(6000);	
+	});
+});
