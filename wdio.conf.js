75651785b1dd2e727056979eb26cbfc081cf29fa
Serggio <oceanite@yandex.ru>
2019-12-26 10:31:37 +0300

    Fix

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,17 +1,18 @@
-var baseUrl;
+// var baseUrl;
 
-if(process.env.SERVER === 'prod') {
-    baseUrl = 'https://www.google.com';
-    } else {
-        baseUrl = 'http://www.webdriveruniversity.com'
-    }
+// if(process.env.SERVER === 'prod') {
+//     baseUrl = 'https://www.google.com';
+//     } else {
+//         baseUrl = 'http://www.webdriveruniversity.com'
+//     }
 
-    var timeout = process.env.DEBUG ? 9999999 : 10000;
+//     var timeout = process.env.DEBUG ? 9999999 : 10000;
 
 
 exports.config = {
+    runner: 'local',
     specs: [
-        'tests/*.js'
+        'test/*.js'
     ],
     //exclude: [
      //   'pageObjects/*_Page.js'
@@ -26,20 +27,23 @@ exports.config = {
 
     sync: true,
     // Level of logging verbosity: silent | verbose | command | data | result | error
-    logLevel: 'silent',
+    logLevel: 'error',
     coloredLogs: true,
     deprecationWarnings: true,
     bail: 0,
-    screenshotPath: './errorShots/',
-    baseUrl: baseUrl,
+    //screenshotPath: './errorShots/',
+    baseUrl: 'http://www.webdriveruniversity.com',
     waitforTimeout: 10000,
     connectionRetryTimeout: 90000,
     connectionRetryCount: 3,
     services: ['selenium-standalone'],
+    //services: ['chromedriver'],
     framework: 'mocha',
+    reporters: ['spec'],
     mochaOpts: {
         ui: 'bdd',
-        timeout: timeout
+        timeout: 60000,
+        compilers: ['js:@babel/register']
     },
 
     before: function (capabilities, specs) {
