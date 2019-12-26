6620dc018491d4481fc5738833e553cb0b4a557c
Serggio <oceanite@yandex.ru>
2019-12-26 11:17:41 +0300

    Adding custom command

--- wdio.conf.js
+++ wdio.conf.js
@@ -51,5 +51,13 @@ exports.config = {
     	require('@babel/register');
         expect = require('chai').expect;
         //should = require('chai').should();
+
+        browser.addCommand("getUrlAndTitle", function () {
+            // `this` refers to the `browser` scope
+            return {
+                url: this.getUrl(),
+                title: this.getTitle()
+            };
+        });        
     },
  }
